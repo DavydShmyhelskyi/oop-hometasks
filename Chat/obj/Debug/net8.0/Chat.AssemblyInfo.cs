a83c49924a9713fa87575ae6a201de51ab335861
DavydShmyhelskyi <shmyhelskyidavid58@gmail.com>
2025-12-01 14:08:42 +0200

    ALL DONE

--- Chat/obj/Debug/net8.0/Chat.AssemblyInfo.cs
+++ Chat/obj/Debug/net8.0/Chat.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Chat")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f9adf4e48c4170b1286d20dd49b1fe426aaaf75")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb54974b712c319cbab07d522b82ad8cfbc187ad")]
 [assembly: System.Reflection.AssemblyProductAttribute("Chat")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Chat")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
